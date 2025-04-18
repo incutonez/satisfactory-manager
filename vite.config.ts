9df112773f89f60a34345a68a210de79dcc238b8
Jef <incutonez@hotmail.com>
2025-04-18 11:07:13 -0400

    chore: Fixing deployment URLs (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path";
-import svgLoader from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import svgLoader from "vite-plugin-svgr";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgLoader(), tailwindcss()],
-  resolve: {
-    alias: [{
-      // Add ability to use @ to represent the root dir being src
-      find: "@",
-      replacement: path.resolve(path.resolve(), "./src"),
-    }],
-  },
-})
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [react(), svgLoader(), tailwindcss()],
+		base: mode === "GitHubPages" ? "/satisfactory-manager/" : "",
+		resolve: {
+			alias: [{
+				// Add ability to use @ to represent the root dir being src
+				find: "@",
+				replacement: path.resolve(path.resolve(), "./src"),
+			}],
+		},
+	};
+});
